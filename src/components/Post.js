88f4901a8e04c602d904ef456fee01da3261b7dc
yegorich75 <yegorich75@gmail.com>
2019-05-28 06:49:06 +0300

    post component props

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,6 +4,9 @@ export default class Post extends Component {
     render() {
         return (
             <div className="post">
+
+                <img src={this.props.src} alt={this.props.alt}></img>
+                <div className="post__name">Some account</div>
                 
             </div>
         )
